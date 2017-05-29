1a17bcd30033f63b90ab22eb3887ff8f9c95607b
Ebuka Hills <flyg101@gmail.com>
2017-05-29 13:35:24 +0100

    update live version

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'client', 'index.html'));
 });
 
